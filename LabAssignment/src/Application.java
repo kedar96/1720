f05942b93e0cdd393c2d6944d5e892d299f76886
kedar96 <prabhugaonkerkedar@gmail.com>
2018-07-11 16:05:21 +0530

    adding initial

---
+++ LabAssignment/src/Application.java
@@ -0,0 +1,18 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int arr[]= {12,5,6,8,1,45,3};
+		 BubbleSort bSort=new BubbleSort();
+		 bSort.setData(arr);
+		 System.out.println("Before sorting");
+		 bSort.printArray(arr);
+		 bSort.sort(arr);
+		 System.out.println("After sorting in ascending order");
+		 bSort.printArray(arr);
+		 
+		 BubbleSort.sort();
+	}
+
+}
---
+++ LabAssignment/src/BubbleSort.java
@@ -0,0 +1,49 @@
+
+public class BubbleSort {
+	private int data[];
+	
+	
+	public void sort(int[] arr )
+	{
+		 int n=arr.length;
+		 int i,j,temp;
+		 for(i=0;i<n-1;i++)
+		 {
+			 for(j=0;j<n-i-1;j++)
+			 {
+				 if(arr[j]>arr[j+1])
+				 {
+					 	temp = arr[j];
+	                    arr[j] = arr[j+1];
+	                    arr[j+1] = temp;
+				 }
+			 }
+		 }
+	}
+
+
+	public void setData(int[] arr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	public void printArray(int[] arr) {
+		// TODO Auto-generated method stub
+		int i; 
+		for (i=0; i<arr.length; ++i)
+	     {
+			 System.out.println(arr[i] + " ");
+	     }
+	}
+
+
+	public static void sort() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
