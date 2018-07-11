0947d4e7d170ebb4c3be438b8dadccbb30818728
kedar96 <prabhugaonkerkedar@gmail.com>
2018-07-11 17:00:50 +0530

    updating

--- LabAssignment/src/Application.java
+++ LabAssignment/src/Application.java
@@ -1,7 +1,7 @@
 public class Application {
 	
 	public static void main(String args[]) {
-		int []arr = {10, 7,34,56,2, 15, 1,22};
+		int []arr = {12,39,34,56,2, 15, 1,22};
 	
 		BubbleSort bSort = new BubbleSort();
 		bSort.setArr(arr);
