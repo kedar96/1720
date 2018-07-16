67e6a6b939453ac1104c24ffadd60c1b38000deb
kedar96 <prabhugaonkerkedar@gmail.com>
2018-07-16 15:24:06 +0530

    hello

--- Factorial/src/FactorialMain.java
+++ Factorial/src/FactorialMain.java
@@ -3,7 +3,6 @@ public class FactorialMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int total=0;
 		Fact f1=new Fact();
 		f1.setNum(7);
 		f1.factorial();
