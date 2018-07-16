f5f890f69e5bcf25889d849c1ad3a4553ecb511e
kedar96 <prabhugaonkerkedar@gmail.com>
2018-07-16 16:47:48 +0530

    update

--- Factorial/src/FactorialMain.java
+++ Factorial/src/FactorialMain.java
@@ -5,7 +5,7 @@ public class FactorialMain {
 		// TODO Auto-generated method stub
 		int total=0;
 		Fact f1=new Fact();
-		f1.setNum(7);
+		f1.setNum(6);
 		total=f1.factorial((f1.getNum()));
 		System.out.println("Factorial of "+f1.getNum()+" is: "+total);
 	}
